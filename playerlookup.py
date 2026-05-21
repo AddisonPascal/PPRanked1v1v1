abd0a19fa30574f284c34b3a2eaddc1f2e17ca4d
Addison Djatschenko <addisondj@hotmail.com>
2026-05-22 09:35:02 +1000

    Re-enabled playerlookup

--- playerlookup.py
+++ playerlookup.py
@@ -1,8 +1,8 @@
 
 import csv
 
-#with open('edb.csv', 'r') as f: d=dict(csv.reader(f))
-d=None
+with open('edb.csv', 'r') as f: d=dict(csv.reader(f))
+#d=None
 
 def ign(disc):
     return d[str(disc)]
